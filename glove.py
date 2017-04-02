07d6e0db7a8db2bfafc3a1a89559e321751edace
Kai <3130000495@zju.edu.cn>
2017-04-01 21:10:08 -0700

    finished glove

--- glove.py
+++ glove.py
@@ -34,16 +34,57 @@ class GloVe(object):
         self._learning_rate = options.learning_rate
         self._batch_size = options.batch_size
         self._concurrent_steps = options.concurrent_steps
+        self._learning_rate = options.learning_rate
         self._vocab_size = 0
+        self._num_lines = 1
+        self._batch_per_epoch = self._num_lines / self._batch_size
+        self._x_max = 100
+        self._alpha = 0.75
         self._session = session
+        self.build_train_graph()
+        self.saver = tf.train.Saver()
 
-    def forward(self,target,context):
 
-        #emb [vocab_size,emb_dim]
-        emb = tf.Variable(
+    def build_train_graph(self):
+
+        target = tf.placeholder(tf.int32,shape=[self._batch_size])
+        context = tf.placeholder(tf.int32,shape=[self._batch_size])
+        label = tf.placeholder(tf.int32,shape=[self._batch_size,1])
+        # emb_w [vocab_size,emb_dim]
+        target_emb_w = tf.Variable(
             tf.random_uniform(
-                [self._vocab_size, self._embedding_size], -0.5/self._embedding_size, 0.5/self._embedding_size),
-            name="emb")
+                [self._vocab_size, self._embedding_size], -0.5 / self._embedding_size, 0.5 / self._embedding_size),
+            name="target_emb_w")
+
+        context_emb_w = tf.Variable(
+            tf.random_uniform(
+                [self._vocab_size, self._embedding_size], -0.5 / self._embedding_size, 0.5 / self._embedding_size),
+            name="context_emb_w")
+
+        target_emb_b = tf.Variable(tf.zeros([self._vocab_size]), name="target_emb_w")
+        context_emb_b = tf.Variable(tf.zeros([self._vocab_size]), name="context_emb_w")
+
+        target_w = tf.nn.embedding_lookup(target_emb_w, target)
+        context_w = tf.nn.embedding_lookup(context_emb_w, context)
+
+        target_b = tf.nn.embedding_lookup(target_emb_b, target)
+        context_b = tf.nn.embedding_lookup(context_emb_b, context)
+
+        diff = tf.matmul(target_w, context_w, transpose_b=True) - target_b - context_b - label
+        fdiff = tf.minium(
+            diff,
+            tf.pow(label / self._x_max, self._alpha) * diff
+        )
+        loss = diff * fdiff
+
+        if np.isnan(loss):
+            print("current loss IS NaN. This should never happen :)")
+            sys.exit(1)
+        self._loss = loss
+
+        self.global_step = tf.Variable(0, name="global_step")
+
+        self._optimizer = tf.train.AdagradOptimizer(self._learning_rate).minimize(loss, global_step=self.global_step)
 
     def generate_batch(self, i):
         target = []
@@ -62,6 +103,23 @@ class GloVe(object):
                 label.append(int(line[2]))
         return target, context, label
 
+    def init(self):
+        tf.global_variables_initializer().run()
+        print('Initialized')
+
+    def run(self):
+        average_loss = 0
+        for step in xrange(self._batch_per_epoch):
+            batch_target,batch_context,batch_label = self.generate_batch(step)
+            feed_dict = {target: batch_target,context: batch_context,label:batch_label}
+            _, loss_val = self._session.run([self._optimizer, self._loss], feed_dict=feed_dict)
+            average_loss += loss_val
+            if step % 2000 == 0:
+                if step > 0:
+                    average_loss /= 2000
+                # The average loss is an estimate of the loss over the last 2000 batches.
+                print('Average loss at step ', step, ': ', average_loss)
+                average_loss = 0
 
 def main(_):
     if not FLAGS.save_path or not FLAGS.train_data:
@@ -70,7 +128,16 @@ def main(_):
 
     with tf.Graph().as_default(), tf.Session() as session:
         model = GloVe(FLAGS, session)
-
+        model.init()
+        for epoch in xrange(FLAGS.epochs_to_train):
+            model.run()
+            if (epoch + 1) % 10 == 0:
+                model.saver.save(session,
+                                 os.path.join(FLAGS.save_path, "model.ckpt"),
+                                 global_step=model.global_step)
+        model.saver.save(session,
+                         os.path.join(FLAGS.save_path, "model.ckpt"),
+                         global_step=model.global_step)
 
 if __name__ == "__main__":
     tf.app.run()
