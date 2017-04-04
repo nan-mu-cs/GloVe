6c2edb6cd0cf02b903c8531b807ad01f30e39931
Kai <3130000495@zju.edu.cn>
2017-04-03 21:24:40 -0700

    modified output

--- run.sh
+++ run.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
 
 SAVE_PATH=./data/
-TRAIN_DATA=./data/cooccur_matrix.tfrecord
+TRAIN_DATA=./data/cooccur_matrix.csv
 EMBEDDING_SIZE=200
-EPOCHS=15
+EPOCHS=2
 LEARNING_RATE=0.05
-BATCH_SIZE=5000
-VOCAB_SIZE=18497
+BATCH_SIZE=1000
 MATRIX_SIZE=17724348
+CONCURRENT_STEPS=2
+LOAD_DATA_PER_TIME=100
+VOCAB_DATA=./data/vocab.txt
+EVAL_DATA=./data/questions-words.txt
 
 python glove.py \
     --save_path=$SAVE_PATH \
     --train_data=$TRAIN_DATA \
+    --vocab_data=$VOCAB_DATA \
+    --eval_data=$EVAL_DATA \
     --embedding_size=$EMBEDDING_SIZE \
     --epochs_to_train=$EPOCHS \
     --learning_rate=$LEARNING_RATE \
     --batch_size=$BATCH_SIZE \
-    --vocab_size=$VOCAB_SIZE \
-    --matrix_size=$MATRIX_SIZE
+    --matrix_size=$MATRIX_SIZE \
+    --concurrent_steps=$CONCURRENT_STEPS \
+    --load_data_per_time=$LOAD_DATA_PER_TIME
